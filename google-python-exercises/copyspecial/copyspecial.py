ec0c6317dbce33aaa8102137e02e106470ae3cb6
Ermanno Pirotta <ermanno.pirotta@gmail.com>
2017-11-10 11:14:40 +0100

    add copyspecial solution

--- google-python-exercises/copyspecial/copyspecial.py
+++ google-python-exercises/copyspecial/copyspecial.py
@@ -10,15 +10,52 @@ import sys
 import re
 import os
 import shutil
-import commands
+import zipfile
 
 """Copy Special exercise
 """
 
+""" The goal of this program is to copy all files that are contained in
+the provided list of directories to another directory or zip them to a zip file"""
 # +++your code here+++
 # Write functions and modify main() to call them
 
+# a special file is one with __w+__ pattern
+def find_special_files(from_dirs):
+    special_files = []
 
+    for directory in from_dirs:
+        # skip non existing directories
+        if os.path.exists(directory):
+            filenames = os.listdir(directory)
+            for name in filenames:
+                special_file_match = re.search(r'\w*__\w+__.*', name)
+                if special_file_match:
+                    special_files.append(os.path.join(directory, special_file_match.group(0)))
+    return special_files
+
+def copy_to_dir(from_dirs, todir):
+    print("copying files from " + str(from_dirs) + "to " + str(todir))
+    if not os.path.exists(todir):
+        os.mkdir(todir)
+
+    files_to_copy = find_special_files(from_dirs)
+
+    for file_to_copy in files_to_copy:
+        print("processing file" + filename)
+        shutil.copy(file_to_copy, todir)
+
+def zip_to_file(from_dirs, tozip):
+    print("zipping files from " + str(from_dirs) + "to " + str(tozip))
+    files_to_copy = find_special_files(from_dirs)
+
+    zip_file = zipfile.ZipFile(tozip,'w')
+
+    for filename in files_to_copy:
+        print("processing file" + filename)
+        zip_file.write(filename)
+
+    zip_file.close()
 
 def main():
   # This basic command line argument parsing code is provided.
@@ -28,7 +65,7 @@ def main():
   # which is the script itself.
   args = sys.argv[1:]
   if not args:
-    print ("usage: [--todir dir][--tozip zipfile] dir [dir ...]");
+    print("usage: [--todir dir][--tozip zipfile] dir [dir ...]");
     sys.exit(1)
 
   # todir and tozip are either set from command line
@@ -45,8 +82,15 @@ def main():
     del args[0:2]
 
   if len(args) == 0:
-    print ("error: must specify one or more dirs")
+    print("error: must specify one or more dirs")
     sys.exit(1)
+
+  if todir:
+      copy_to_dir(args, todir)
+  elif tozip:
+      zip_to_file(args,tozip)
+  else:
+      print("error: must specify either --todir or --tozip arguments")
 
   # +++your code here+++
   # Call your functions
